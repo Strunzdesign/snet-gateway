50d83b2a629d21cb1ba4c306cb26418e773a6120
Florian Evers <florian-evers@gmx.de>
2017-01-26 23:39:22 +0100

    Added the autogenerated file config.h to the repository

---
+++ src/Config.h
@@ -0,0 +1,5 @@
+// The configured options and settings for the s-net(r) gateway
+#include "HdlcdConfig.h"
+#include "SnetConfig.h"
+#define SNET_GATEWAY_VERSION_MAJOR "1"
+#define SNET_GATEWAY_VERSION_MINOR "4pre"
