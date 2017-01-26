e06ff8eef7df999554c59277a0a6f3b1177fd765
Florian Evers <florian-evers@gmx.de>
2017-01-26 23:48:10 +0100

    Changed version number to v1.5pre for further development and added licence

--- src/Config.h
+++ src/Config.h
@@ -1,5 +1,28 @@
+/**
+ * \file      config.h
+ * \brief     
+ * \author    Florian Evers, florian-evers@gmx.de
+ * \copyright GNU Public License version 3.
+ *
+ * The HDLC Deamon implements the HDLC protocol to easily talk to devices connected via serial communications.
+ * Copyright (C) 2016  Florian Evers, florian-evers@gmx.de
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // The configured options and settings for the s-net(r) gateway
 #include "HdlcdConfig.h"
 #include "SnetConfig.h"
 #define SNET_GATEWAY_VERSION_MAJOR "1"
-#define SNET_GATEWAY_VERSION_MINOR "4"
+#define SNET_GATEWAY_VERSION_MINOR "5pre"
